606dfb7795a6f0e4269db851301190bd651dcef5
Shrisan <shrisan0083@gmail.com>
2020-11-26 17:20:29 -0600

    manager update added

--- app/src/main/java/com/example/maintanancebuddy/Update_Manager_Information.kt
+++ app/src/main/java/com/example/maintanancebuddy/Update_Manager_Information.kt
@@ -1,15 +1,11 @@
 package com.example.maintanancebuddy
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.fragment_manager__profile.*
+import kotlinx.android.synthetic.main.fragment_update__manager__information.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,14 +14,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Manager_Profile.newInstance] factory method to
+ * Use the [Update_Manager_Information.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Manager_Profile : Fragment() {
+class Update_Manager_Information : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    //private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,14 +28,13 @@ class Manager_Profile : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        update_manager_info()
-        signout()
+        dont_save_manager_changes()
+        save_manager_info_changes()
     }
 
     override fun onCreateView(
@@ -48,7 +42,7 @@ class Manager_Profile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manager__profile, container, false)
+        return inflater.inflate(R.layout.fragment_update__manager__information, container, false)
     }
 
     companion object {
@@ -58,12 +52,12 @@ class Manager_Profile : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Manager_Profile.
+         * @return A new instance of fragment Update_Manager_Information.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Manager_Profile().apply {
+            Update_Manager_Information().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
@@ -71,30 +65,14 @@ class Manager_Profile : Fragment() {
             }
     }
 
-    private fun update_manager_info(){
-        updateInfoManager.setOnClickListener{
-            it.findNavController().navigate(R.id.update_Manager_Information)
-        }
-    }
-    private fun signout()
-    {
-        manager_signout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            cleardata()
-            val intent = Intent(activity, LoginActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
-        }
-    }
-    private fun cleardata()
-    {
-        val preferences = activity
-            ?.getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
-        var editor= preferences?.edit()
-        if (editor != null) {
-            editor.clear()
-            editor.commit()
+    private fun dont_save_manager_changes(){
+        cancel_Manager_Changes.setOnClickListener{
+            fragmentManager?.popBackStack()
         }
     }
 
+    //Need to implement
+    private fun save_manager_info_changes(){
+
+    }
 }
