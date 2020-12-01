866a51568ad924b323d464ee52ccec9b28fa5024
aviaryal <58341812+aviaryal@users.noreply.github.com>
2020-11-30 22:08:35 -0600

    Register bug fix

--- app/src/main/java/com/example/maintanancebuddy/RegisterActivity.kt
+++ app/src/main/java/com/example/maintanancebuddy/RegisterActivity.kt
@@ -48,6 +48,12 @@ class RegisterActivity : AppCompatActivity(){
         {
             createAccount()
         }
+        already_have_account_textview.setOnClickListener(){
+            val intent = Intent(this,LoginActivity::class.java);
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
     }
 
 
